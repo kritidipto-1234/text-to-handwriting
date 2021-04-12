1e9a174a6aa7981b3331c6c6f9aa2225b26d37e9
Kritidipto Ghosh <kritidiptodgp.ghosh@gmail.com>
2021-04-12 14:44:33 +0530

    some changes

--- src/js/image.js
+++ src/js/image.js
@@ -8,20 +8,27 @@ import {pageHeight,pageWidth} from './config.js';
 
 screenshotBtn.addEventListener('click',function()
 {
-    [...document.querySelectorAll('.pageContainer')].forEach(async function(page)
+    [...document.querySelectorAll('.pageContainer')].forEach(function(page)
         {
             page.style.border='none';
-            const canvas=await html2canvas(page,{scale: 3,height:pageHeight,width:pageWidth});
-            const markup=
-            `<div class="imageContainer">
-            <button class="deleteImgBtn" type="button">x</button>
-            <img  src="${canvas.toDataURL('image/jpeg',3)}" class="canvasImg" alt="">
-            <a class="downloadBtn" download='handwritten.jpeg' href="${canvas.toDataURL('image/jpeg', 1)}" t>Download Image</a>
-            </div>`;
-            console.log(page);
-            imageList.insertAdjacentHTML('beforeend',markup);
-            page.style.border='1px solid black';
+            html2canvas(page,{scale: 3,height:pageHeight,width:pageWidth})
+            .then
+            ((canvas)=>
+                {
+                    const markup=
+                    `<div class="imageContainer">
+                    <button class="deleteImgBtn" type="button">x</button>
+                    <img  src="${canvas.toDataURL('image/jpeg',3)}" class="canvasImg" alt="">
+                    <a class="downloadBtn" download='handwritten.jpeg' href="${canvas.toDataURL('image/jpeg', 1)}" t>Download Image</a>
+                    </div>`;
+                    imageList.insertAdjacentHTML('beforeend',markup);
+                    page.style.border='1px solid black';
+                }
+            )
         });
+    const {left,top}=imageList.getBoundingClientRect();
+    console.log(left+pageXOffset,top+pageYOffset);
+    window.scrollTo({left:left+pageXOffset,top:top+pageYOffset,behavior:'smooth'});
 });
 
 imageList.addEventListener('click',function(e)
