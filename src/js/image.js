89d18fcf9d256d2da1d53b3c0068aa6a557bb456
Kritidipto Ghosh <kritidiptodgp.ghosh@gmail.com>
2021-06-26 22:03:26 +0530

    some changes

--- src/js/image.js
+++ src/js/image.js
@@ -1,38 +1,52 @@
-const screenshotBtn=document.querySelector('.screenshotBtn');
-const imageList=document.querySelector('.imageList');
+const screenshotBtn = document.querySelector(".screenshotBtn");
+const imageList = document.querySelector(".imageList");
+const deleteAllImgBtn = document.querySelector(".deleteAllImgBtn");
 
 import "regenerator-runtime/runtime.js";
-import html2canvas from 'html2canvas';
-import {pageHeight,pageWidth} from './config.js';
+import html2canvas from "html2canvas";
+import { pageHeight, pageWidth } from "./config.js";
 
+screenshotBtn.addEventListener("click", async function () {
+  for (let page of [...document.querySelectorAll(".pageContainer")]) {
+    page.style.border = "none";
+    const canvas = await html2canvas(page, {
+      scale: 3,
+      scrollX: -window.scrollX,
+      scrollY: -window.scrollY,
+      height: pageHeight,
+      width: pageWidth,
+    });
 
-screenshotBtn.addEventListener('click',async function()
-{
-    for (let page of [...document.querySelectorAll('.pageContainer')])
-    {
-        page.style.border='none';
-        const canvas=await html2canvas(page,{scale: 3,scrollX: -window.scrollX,scrollY: -window.scrollY,height:pageHeight,width:pageWidth})
-            
-        const newDiv=document.createElement('div');
-        newDiv.classList.add('imageContainer');
-        imageList.appendChild(newDiv);
-        newDiv.innerHTML=
-        `
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("imageContainer");
+    imageList.appendChild(newDiv);
+    newDiv.innerHTML = `
         <button class="deleteImgBtn" type="button">X</button>
-        <img  src="${canvas.toDataURL('image/jpeg',3)}" class="canvasImg" alt="">
-        <a class="downloadBtn" download='handwritten.jpeg' href="${canvas.toDataURL('image/jpeg', 1)}" t>Download Image</a>
+        <img  src="${canvas.toDataURL(
+          "image/jpeg",
+          3
+        )}" class="canvasImg" alt="">
+        <a class="downloadBtn" download='handwritten.jpeg' href="${canvas.toDataURL(
+          "image/jpeg",
+          1
+        )}" t>Download Image</a>
         `;
-        page.style.border='1px solid black';
-    };
-    const {left,top}=imageList.getBoundingClientRect();
-    window.scrollTo({left:left+pageXOffset,top:top+pageYOffset,behavior:'smooth'});
+    page.style.border = "1px solid black";
+  }
+  const { left, top } = imageList.getBoundingClientRect();
+  window.scrollTo({
+    left: left + pageXOffset,
+    top: top + pageYOffset,
+    behavior: "smooth",
+  });
 });
 
-imageList.addEventListener('click',function(e)
-{
-    if (!e.target.closest('.deleteImgBtn')) return;
-    const currImage=e.target.closest('.imageContainer');
-    currImage.remove();
-})
+imageList.addEventListener("click", function (e) {
+  if (!e.target.closest(".deleteImgBtn")) return;
+  const currImage = e.target.closest(".imageContainer");
+  currImage.remove();
+});
 
-
+deleteAllImgBtn.addEventListener("click", function () {
+  imageList.innerHTML = "";
+});
